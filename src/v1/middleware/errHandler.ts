adb354392ec19db2f10c5432909952873ccdb74c
permaficus <abukhalif2019@gmail.com>
2024-05-04 19:30:19 +0700

    feat(prisma-error): add custom prisma error

--- src/v1/middleware/errHandler.ts
+++ src/v1/middleware/errHandler.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 export const badRequest = async ( err: any, req: Request, res: Response ) => {
     if (err instanceof SyntaxError && 'body' in err) {
@@ -9,4 +9,16 @@ export const badRequest = async ( err: any, req: Request, res: Response ) => {
         }).end();
         return;
     }
+}
+export const PathNotFound = async (req: Request, res: Response, next: NextFunction) => {
+    res.status(404);
+    next(new Error(`Path Not Found - ${req.originalUrl}`));
+}
+export const errHandler = async (err: any, req: Request, res: Response, next: NextFunction ) => {
+    const statusCode = res.statusCode !== 200 ? res.statusCode : 500
+    res.status(statusCode).json({
+        status: statusCode == 400 ? 'ERR_BAD_REQUEST' : 'ERR_BAD_SERVICE',
+        code: statusCode,
+        details: err.message
+    }).end();
 }
